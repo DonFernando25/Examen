151d19d39f95c464fa5c4f02907d7f10fd030022
DonFernando25 <fe.arevaloa@duocuc.cl>
2024-11-11 22:21:11 -0300

    11-11-22:21

--- Django/Django/Estrella/tests.py
+++ Django/Django/Estrella/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from .models import Usuario, Producto  # Importa los modelos que usarás
 
-# Create your tests here.
+
+class UsuarioModelTest(TestCase):
+    def test_crear_usuario(self):
+        usuario = Usuario.objects.create(
+            nombre="Juan", apellido="Pérez", nombre_usuario="juanp", correo="juanp@example.com", contrasena="password123")
+        self.assertEqual(usuario.nombre, "Juan")
+
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'nombre_usuario': 'juanp',
+            'contrasena': 'password123'
+        }
+        Usuario.objects.create(**self.credentials)
+
+    def test_login(self):
+        response = self.client.post('/Estrella/IniSe', self.credentials, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+
+class CarritoTest(TestCase):
+    def test_agregar_producto(self):
+        producto = Producto.objects.create(nombre="Camiseta", precio=35000)
+        response = self.client.post('/carrito/agregar', {'producto_id': producto.id})
+        carrito = self.client.session['carrito']
+        self.assertIn(str(producto.id), carrito)
+
+
+import time
+
+class CargaPaginaTest(TestCase):
+    def test_tiempo_respuesta(self):
+        start_time = time.time()
+        self.client.get('/Estrella/Tienda')
+        tiempo_respuesta = time.time() - start_time
+        self.assertLess(tiempo_respuesta, 2)
